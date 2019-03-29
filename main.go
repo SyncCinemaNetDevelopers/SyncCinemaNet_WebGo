31e19bdd57d267b77673fd5a64aa2d44c91dd34f
gregv2 <gaml.vip123@gmail.com>
2019-03-29 14:36:06 +0300

    Add files

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r  *http.Request)  {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
