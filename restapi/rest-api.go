e0bace7323b9389d24ed04e4be6ea9327a7f8656
spanditime <spanditime@gmail.com>
2019-05-15 09:00:06 +0700

    change Handle to HandleFunc

--- restapi/rest-api.go
+++ restapi/rest-api.go
@@ -47,17 +47,17 @@ func deleteRoom(w http.ResponseWriter, r *http.Request) {
 }
 
 func restAPIInitialize(r *mux.Router) {
-	r.Handle("/api/{ver}/servers", getServersList).Methods("GET")
-	r.Handle("/api/{ver}/servers", addServerToList).Methods("POST")
+	r.HandleFunc("/api/{ver}/servers", getServersList).Methods("GET")
+	r.HandleFunc("/api/{ver}/servers", addServerToList).Methods("POST")
 
-	r.Handle("/api/{ver}/servers/{id}", getServerInfo).Methods("GET")
-	r.Handle("/api/{ver}/servers/{id}", updateServerInfo).Methods("PUT")
-	r.Handle("/api/{ver}/servers/{id}", deleteServer).Methods("DELETE")
+	r.HandleFunc("/api/{ver}/servers/{id}", getServerInfo).Methods("GET")
+	r.HandleFunc("/api/{ver}/servers/{id}", updateServerInfo).Methods("PUT")
+	r.HandleFunc("/api/{ver}/servers/{id}", deleteServer).Methods("DELETE")
 
-	r.Handle("/api/{ver}/rooms", getRoomsList).Methods("GET")
-	r.Handle("/api/{ver}/rooms", addRoomToList).Methods("POST")
+	r.HandleFunc("/api/{ver}/rooms", getRoomsList).Methods("GET")
+	r.HandleFunc("/api/{ver}/rooms", addRoomToList).Methods("POST")
 
-	r.Handle("/api/{ver}/rooms/{id}", getRoomInfo).Methods("GET")
-	r.Handle("/api/{ver}/rooms/{id}", updateRoomInfo).Methods("PUT")
-	r.Handle("/api/{ver}/rooms/{id}", deleteRoom).Methods("DELETE")
+	r.HandleFunc("/api/{ver}/rooms/{id}", getRoomInfo).Methods("GET")
+	r.HandleFunc("/api/{ver}/rooms/{id}", updateRoomInfo).Methods("PUT")
+	r.HandleFunc("/api/{ver}/rooms/{id}", deleteRoom).Methods("DELETE")
 }
