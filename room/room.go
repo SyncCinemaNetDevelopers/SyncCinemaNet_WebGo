874b85a228c694921db0037bffe429cdf1eb4242
gregv2 <gaml.vip123@gmail.com>
2019-08-03 18:41:07 +0300

    add Room struct

--- room/room.go
+++ room/room.go
@@ -1,2 +1,12 @@
 package room
 
+import "SyncCinemaNet_WebGo/auth/auth"
+
+
+type Room struct {
+	Room_ID int64
+	Name    string
+	Private bool
+	Author  auth.User
+	userlist map[string]auth.User
+}
