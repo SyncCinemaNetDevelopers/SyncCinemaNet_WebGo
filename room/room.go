1157a59008d2202725cdac016b3e11ae964142b8
gregv2 <gaml.vip123@gmail.com>
2019-08-04 18:00:12 +0300

    correct Room struct

--- room/room.go
+++ room/room.go
@@ -2,12 +2,14 @@ package room
 
 import (
 	"SyncCinemaNet_WebGo/auth"
+	info_of "SyncCinemaNet_WebGo/chat"
 )
 
 type Room struct {
 	ID       int64
 	Name     string
 	Private  bool
+	Chat     info_of.Chat
 	Author   auth.User
 	userlist map[string]auth.User
 	Tags     map[string]string
