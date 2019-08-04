ae82ae76c1e4c35da35a4fa07e83a2066fead942
gregv2 <gaml.vip123@gmail.com>
2019-08-04 13:59:14 +0300

    add Chat and Message structes

--- chat/chat.go
+++ chat/chat.go
@@ -1,1 +1,19 @@
 package chat
+
+import (
+	"SyncCinemaNet_WebGo/auth"
+	"SyncCinemaNet_WebGo/room"
+)
+
+type Chat struct {
+	ID   int64
+	Room room.Room
+}
+
+type Message struct {
+	ID      int
+	Chat_ID Chat
+	Time    string
+	Author  auth.User
+	Text    string
+}
